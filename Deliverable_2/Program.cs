208278e55c519ba0bbb64b98a8e43e0888aaf29f
samee <samee@LAPTOP-2MQK9SK0>
2023-01-19 18:19:30 -0500

    Add project files.

---
+++ Deliverable_2/Program.cs
@@ -0,0 +1,74 @@
+﻿/*
+Author : Fnu Sameer
+Date: 01/19/2023
+Description: Enter a numeric value to get a letter grade
+*/
+
+
+using System;
+using System.Linq.Expressions;
+
+namespace Deliverable_2
+{
+
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What grade do you expect in ISM 4300: ");
+
+            try
+            {
+
+                decimal grade = decimal.Parse(Console.ReadLine());
+
+                string letter;
+
+                if (grade >= 90) 
+                {
+                    letter = "A";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+
+                else if (grade >= 80)
+                {
+                    letter = "B";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+
+                else if (grade >= 70)
+                {
+                    letter = "C";
+                    Console.WriteLine("Your letter grade is: " + letter);
+
+                }
+
+                else if (grade >= 60)
+                {
+                    letter = "D";
+                    Console.WriteLine("Your letter grade is: " + letter);
+                }
+
+                else 
+                {
+                    letter = "F";
+                    Console.WriteLine("Your letter grade is: " + letter);
+
+                }
+
+            }
+
+
+            catch
+            {
+                Console.WriteLine("Enter a numeric value");
+            }
+
+
+        }
+
+        
+    }
+}
